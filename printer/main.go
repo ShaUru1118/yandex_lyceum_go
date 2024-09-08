467cb84461bf360fefc35396670eb575f3c886ec
Albom3 <sargsyan_ae09@mail.ru>
2024-09-08 12:56:21 +0300

    sch :-)

--- printer/main.go
+++ printer/main.go
@@ -1,12 +1,10 @@
 package printer
 
-import "fmt"
-
 func Sum(a, b int) int {
 	return a + b
 }
 
-func main() {
-	a := Sum(1, 3)
-	fmt.Println("result:", a)
-}
+// func main() {
+// 	a := Sum(1, 3)
+// 	fmt.Println("result:", a)
+// }
