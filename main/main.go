037e1dbb86c2a3dc9e1176ad2af01dd2a8448f63
Albom3 <sargsyan_ae09@mail.ru>
2024-09-08 13:45:47 +0300

    sch :-)

--- main/main.go
+++ main/main.go
@@ -1,18 +1,37 @@
 package main
 
-import "fmt"
+import "math"
 
-func main() {
-	var n int
-	fmt.Scan(&n)
-
-	a, b := 0, 1
-
-	for i := 0; i < 10; i++ {
-		for b < n {
-			a, b = b, a+b
-		}
-		fmt.Println(b)
-		a, b = b, a+b
+func CalculateSeriesSum(n int) float64 {
+	s := 0.0
+	for i := 1; i <= n; i++ {
+		s += 1 / float64(i)
 	}
+	return s
+}
+
+func FindIntersection(k1, b1, k2, b2 float64) (float64, float64) {
+	if k1 == k2 {
+		return math.NaN(), math.NaN()
+	}
+	x := (b2 - b1) / (k1 - k2)
+	y := k1*x + b1
+
+	return x, y
+}
+
+func CalculateDigitalRoot(n int) int {
+	sumOfDigits := func(x int) int {
+		sum := 0
+		for x > 0 {
+			sum += x % 10
+			x /= 10
+		}
+		return sum
+	}
+
+	for n >= 10 {
+		n = sumOfDigits(n)
+	}
+	return n
 }
